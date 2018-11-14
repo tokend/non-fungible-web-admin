6dd01b631523d9c8a8d4bd9763f34684c667b2d5
Roman Malyshev <malyshevroman.a@gmail.com>
2018-11-14 21:34:30 +0200

    Add seed auuthentication

--- config/default.env.js
+++ config/default.env.js
@@ -1,10 +1,15 @@
 module.exports = {
   NODE_ENV: '"default"',
-  HORIZON_SERVER: '"https://api.alice.tokend.io"',
-  STORAGE_SERVER: '"https://storage.alice.tokend.io"',
-  KEY_SERVER_ADMIN: '"https://admin.alice.tokend.io/_/adks"',
-  NETWORK_PASSPHRASE: '"TokenD Alice Network!"',
+  HORIZON_SERVER: '"https://api.testnet.tokend.org"',
+  STORAGE_SERVER: '"https://storage.testnet.tokend.org/api"',
+  KEY_SERVER_ADMIN: '"https://admin.testnet.tokend.org/_/adks"',
+  NETWORK_PASSPHRASE: '"TokenD Testnet Network"',
   FEATURES: {
+    /* Enable authentication using seed. Such auth has no 2FA.
+    * WARN: for demo purposes only, could be very dangerous on prod
+    */
+    SEED_AUTH: true,
+
     PHOTO_VERIFICATION: true,
     KYC_REVIEW_MANAGER: false,
     KYC_REQUEST_STATES: [
